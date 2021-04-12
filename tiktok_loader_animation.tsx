7c1299a088c9192a09ad243ecda2454250eab695
Christian <christian@Christians-MBP.fritz.box>
2021-04-12 17:59:41 +0200

    Refactor spinner implementation

--- tiktok_loader_animation.tsx
+++ tiktok_loader_animation.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     Button,
     SafeAreaView,
     StyleSheet,
 
 } from 'react-native';
-import Animated, { Easing, Extrapolate, interpolate, useAnimatedProps, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
-import Svg, {Circle, Defs, ClipPath, G} from 'react-native-svg';
+import Animated, { cancelAnimation, Easing, Extrapolate, interpolate, useAnimatedProps, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
+import Svg, { Circle, Defs, ClipPath, G } from 'react-native-svg';
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedG = Animated.createAnimatedComponent(G);
 
 const GREEN_COLOR = "rgb(66, 221, 240)";
 const RED_COLOR = "rgb(240, 50, 75)";
 const BACKGROUND_COLOR = "rgb(10, 10, 10)";
+
+const VIEWBOX_HEIGHT = 100;
+const VIEWBOX_WIDTH = 100;
+const BASE_DIAMETER = 50;
+const BASE_RADIUS = BASE_DIAMETER / 2;
 
 const styles = StyleSheet.create({
     screen: {
@@ -21,15 +29,6 @@ const styles = StyleSheet.create({
     },
 });
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-const AnimatedG = Animated.createAnimatedComponent(G);
-
-
-
-const VIEWBOX_HEIGHT = 100;
-const VIEWBOX_WIDTH = 100;
-const BASE_DIAMETER = 50;
-const BASE_RADIUS = BASE_DIAMETER / 2;
 
 const calcAnimatedPropsRightToLeft = (sharedValue: Animated.SharedValue<number>, firstPart: boolean) => {
     "worklet";
@@ -60,9 +59,16 @@ const calcAnimatedPropsLefttoRight = (sharedValue: Animated.SharedValue<number>,
 }
 
 
-const TikTokAnimationScreen = ({ navigation }) => {
-
+const TikTokSpinner = () => {
     const animatedValue = useSharedValue(0.0);
+
+    useEffect(() => {
+        animatedValue.value = 0;
+        animatedValue.value = withRepeat(withTiming(2, { duration: 900, easing: Easing.linear }), -1, false);
+        return () => {
+            cancelAnimation(animatedValue);
+        }
+    }, []);
 
     // Group 1 is only visible in the first half of the animation cycle
     const group1Props = useAnimatedProps(() => {
@@ -72,22 +78,22 @@ const TikTokAnimationScreen = ({ navigation }) => {
     });
 
     const group1RightToLeftCircleProps = useAnimatedProps(() => {
-        // position from right to left
+        // animate from right to left
         return calcAnimatedPropsRightToLeft(animatedValue, true);
     });
 
     const group1LeftToRightCircleProps = useAnimatedProps(() => {
-        // position from left to right
+        // animate from left to right
         return calcAnimatedPropsLefttoRight(animatedValue, true);
     });
 
     const group1LeftToRightBackgroundCircleProps = useAnimatedProps(() => {
-        // position from left to right
+        // animate from left to right
         return calcAnimatedPropsLefttoRight(animatedValue, true);
     });
 
     const group1ClipPathProps = useAnimatedProps(() => {
-        // position from right to left
+        // animate from right to left
         return calcAnimatedPropsRightToLeft(animatedValue, true);
     });
 
@@ -101,75 +107,71 @@ const TikTokAnimationScreen = ({ navigation }) => {
     });
 
     const group2LeftToRightCircleProps = useAnimatedProps(() => {
-        // position from left to right
+        // animate from left to right
         return calcAnimatedPropsLefttoRight(animatedValue, false);
     });
 
     const group2RightToLeftCircleProps = useAnimatedProps(() => {
-        // position from right to left
+        // animate from right to left
         return calcAnimatedPropsRightToLeft(animatedValue, false);
     });
 
     const group2LeftToRightBackgroundCircleProps = useAnimatedProps(() => {
-        // position from left to right
+        // animate from left to right
         return calcAnimatedPropsLefttoRight(animatedValue, false);
     });
 
     const group2ClipPathProps = useAnimatedProps(() => {
-        // position from right to left
+        // animate from right to left
         return calcAnimatedPropsRightToLeft(animatedValue, false);
     });
 
+    return (
+        <Svg height="40" width="40" viewBox="0 0 100 100">
+            <Defs>
+                <ClipPath id="group1-background-clippath">
+                    <AnimatedCircle animatedProps={group1ClipPathProps} cy="50" />
+                </ClipPath>
+
+                <ClipPath id="group2-background-clippath">
+                    <AnimatedCircle animatedProps={group2ClipPathProps} cy="50" />
+                </ClipPath>
+            </Defs>
 
 
+            <AnimatedG animatedProps={group1Props}>
+                { /* left circle */}
+                <AnimatedCircle animatedProps={group1LeftToRightCircleProps} cy="50" fill={RED_COLOR} />
+
+                { /* right circle */}
+                <AnimatedCircle animatedProps={group1RightToLeftCircleProps} cy="50" fill={GREEN_COLOR} />
+
+                { /* left circle with background color */}
+                <AnimatedCircle animatedProps={group1LeftToRightBackgroundCircleProps} cy="50" fill={BACKGROUND_COLOR} clipPath="url(#group1-background-clippath)" />
+            </AnimatedG>
+
+
+            <AnimatedG animatedProps={group2Props}>
+
+                { /* second part of animation. left circle. from left right */}
+                <AnimatedCircle animatedProps={group2LeftToRightCircleProps} cy="50" fill={GREEN_COLOR} />
+
+                { /* second part of animation. right circle. from right to left */}
+                <AnimatedCircle animatedProps={group2RightToLeftCircleProps} cy="50" fill={RED_COLOR} />
+
+                { /* left circle with background color */}
+                <AnimatedCircle animatedProps={group2LeftToRightBackgroundCircleProps} cy="50" fill={BACKGROUND_COLOR} clipPath="url(#group2-background-clippath)" />
+
+            </AnimatedG>
+
+        </Svg>
+    );
+};
+
+const TikTokAnimationScreen = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.screen}>
-            
-            <Svg height="40" width="40" viewBox="0 0 100 100">
-                <Defs>
-                    <ClipPath id="group1-background-clippath">
-                        <AnimatedCircle animatedProps={group1ClipPathProps} cy="50" />
-                    </ClipPath>
-
-                    <ClipPath id="group2-background-clippath">
-                        <AnimatedCircle animatedProps={group2ClipPathProps} cy="50" />
-                    </ClipPath>
-                </Defs>
-
-
-                <AnimatedG animatedProps={group1Props}>
-                    { /* left circle */ }
-                    <AnimatedCircle animatedProps={group1LeftToRightCircleProps} cy="50" fill={RED_COLOR} />
-
-                    { /* right circle */ }
-                    <AnimatedCircle animatedProps={group1RightToLeftCircleProps} cy="50" fill={GREEN_COLOR} />
-
-                    { /* left circle with background color */ }
-                    <AnimatedCircle animatedProps={group1LeftToRightBackgroundCircleProps} cy="50" fill={BACKGROUND_COLOR} clipPath="url(#group1-background-clippath)" />
-                </AnimatedG>
-
-
-                <AnimatedG animatedProps={group2Props}>
-                
-                    { /* second part of animation. left circle. from left right */ }
-                    <AnimatedCircle animatedProps={group2LeftToRightCircleProps} cy="50" fill={GREEN_COLOR} />
-
-                    { /* second part of animation. right circle. from right to left */ }
-                    <AnimatedCircle animatedProps={group2RightToLeftCircleProps} cy="50" fill={RED_COLOR} />
-                
-                    { /* left circle with background color */ }
-                    <AnimatedCircle animatedProps={group2LeftToRightBackgroundCircleProps} cy="50" fill={BACKGROUND_COLOR} clipPath="url(#group2-background-clippath)" />
-
-                </AnimatedG>
-
-            </Svg>
-
-            <Button title="Start animation" onPress={() => {
-                animatedValue.value = 0;
-                animatedValue.value =  withRepeat(withTiming(2, { duration: 900, easing: Easing.linear }), -1, false);
-            }}></Button>
-
-
+            <TikTokSpinner />
         </SafeAreaView>
     );
 }
